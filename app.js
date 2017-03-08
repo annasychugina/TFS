508c5dc09ad2117f3b8ed6192e460d15723f640b
sychuginaanna <sychuginaanna@ya.ru>
2017-03-08 09:35:35 +0300

    hm1

---
+++ app.js
@@ -0,0 +1,19 @@
+let str1 = prompt('Введите первую строку');
+let str2 = prompt('Введите вторую строку');
+
+const sort = (str) => str.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+
+const isAnagram = (str1, str2) => {
+    if (str1.trim() === str2.trim()) {
+        alert('не являются анаграммами: строки одинаковы');
+        return false;
+    }
+    if (sort(str1) === sort(str2)) {
+        alert('анаграммы');
+        return false;
+    }
+
+    alert('не являются анаграммами');
+};
+
+isAnagram(str1, str2);
