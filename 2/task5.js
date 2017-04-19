34a531917420e7d6760b2c33a5eddf45ec10c709
AS <sychuginaanna@ya.ru>
2017-04-19 06:32:46 +0300

    task5 universal function

--- 2/task5.js
+++ 2/task5.js
@@ -1,19 +1,34 @@
-function sum(arg) {
-    let result = function(b) {
-        return sum(arg + b);
-    };
+// function sum(arg) {
+//     let result = function(b) {
+//         return sum(arg + b);
+//     };
 
-    result.toString = function() {
-        return arg;
-    };
+//     result.toString = function() {
+//         return arg;
+//     };
 
-    result.valueOf = function() {
-        return arg;
-    };
+//     result.valueOf = function() {
+//         return arg;
+//     };
 
-    return result;
+//     return result;
+// }
+
+// console.log(sum(1)(2));
+// console.log(sum(1)(2)(3));
+// console.log(sum(3)(5)(6)(0));
+
+function sum(x) {
+	function f(y) {
+
+		return (arguments.length === 0) ? x : sum(x+y);
+	}
+
+	f.toString = function() {return x};
+	
+	return f;
 }
 
-console.log(sum(1)(2));
-console.log(sum(1)(2)(3));
-console.log(sum(3)(5)(6)(0));
+alert(sum(2)(3)(12)( ));
+// alert(sum(1)(2));
+
